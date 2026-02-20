d7107b8817308b6d588b27b4e9a8de646d4409d1
Veronika-Bortkova <shishiga79@gmail.com>
2026-02-20 23:04:39 +0200

    br point 2 end

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ form.addEventListener("submit",function (ev){
                 if (!/^[\p{L}\p{N}][\p{L}\p{N}]*\s*$/u.test(Name_Value[0]) || !/^\s*[\p{L}\p{N}]+$/u.test(Name_Value[1])){//делаю проверку строк до знака равно и после. Строка находящаяся до знака "=" должна начинаться только с численно-буквенных символов, далее содержать любое (в том числе ноль) кол-во буквенно-числовых символов, конец строки - любое кол-во пробелов. После знака "=" строка должна начинаться с любого кол-ва пробелов (в том числе ноль - ни одного пробела), далее любое кол-во буквенно-численных символов, но не менее одного символа. Юникод флаг для того чтобы работало для букв и чисел любого алфавита.
                     DivInCorrectFormat.innerText = "Enter the information in the correct format";
                  } else{
-                    DivInCorrectFormat.innerText ="";//убираю сообщение об ошибке
+                    DivInCorrectFormat.innerText =" ";//убираю сообщение об ошибке
                     let optionPair = document.createElement("option");
                     optionPair.innerText =(Name_Value[0]+"="+Name_Value[1]).replaceAll(" ","");//формирую строку, которую кладем в список, убираю пробелы перед и до знака "="
                     selectNameValue.append(optionPair);
