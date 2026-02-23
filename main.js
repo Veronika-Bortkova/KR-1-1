263cd198d8bd3473b43351692543b35aa88e96b5
Veronika-Bortkova <shishiga79@gmail.com>
2026-02-23 15:41:52 +0200

    finish-1

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ let input = document.getElementById("InputNameValuePair");
 let divDeleteText = document.getElementById("deleteText");
 let pTexForFieldAndroid = document.getElementById("texForFieldAndroid");
 let buttonSortByName = document.getElementById("SortByName");
+let buttonSortByValue = document.getElementById("SortByValue");
+let buttonDelete = document.getElementById("delete");
 let select = document.getElementById("myList");
 
 
@@ -47,10 +49,10 @@ form.addEventListener("submit",function (ev){
                 DivInCorrectFormat.innerText = "Enter the information in the correct format";// если в строке нет знака "=" или их больше одного, выдаю сообщение о том, чтобы ввели текст в правильном формате
             } else {
                 let Name_Values = value.split("=");
-                if (!/^[\p{L}\p{N}][\p{L}\p{N}]*\s*$/u.test(Name_Values[0]) || !/^\s*[\p{L}\p{N}]+$/u.test(Name_Values[1])){//делаю проверку строк до знака равно и после. Строка находящаяся до знака "=" должна начинаться только с численно-буквенных символов, далее содержать любое (в том числе ноль) кол-во буквенно-числовых символов, конец строки - любое кол-во пробелов. После знака "=" строка должна начинаться с любого кол-ва пробелов (в том числе ноль - ни одного пробела), далее любое кол-во буквенно-численных символов, но не менее одного символа. Юникод флаг для того чтобы работало для букв и чисел любого алфавита.
+                if (!/^[\p{L}\p{N}][\p{L}\p{N}]*\s*$/u.test(Name_Values[0]) || !/^\s*[\p{L}\p{N}]+$/u.test(Name_Values[1])){//делаю проверку строк до знака равно и после. Строка находящаяся до знака "=" должна начинаться только с буквенно-цифровых символов, далее содержать любое (в том числе ноль) кол-во буквенно-цифровых символов, конец строки - любое кол-во пробелов. После знака "=" строка должна начинаться с любого кол-ва пробелов (в том числе ноль - ни одного пробела), далее любое кол-во буквенно-цифровых символов, но не менее одного символа. Юникод флаг для того чтобы работало для букв и чисел любого алфавита.
                     DivInCorrectFormat.innerText = "Enter the information in the correct format";
                  } else{
-                    DivInCorrectFormat.innerText =" ";//убираю сообщение об ошибке
+                    DivInCorrectFormat.innerText =" ";//убираю сообщение о том, чтобы ввели текст в правильном формате
                     let optionPair = document.createElement("option");
                     optionPair.innerText =(Name_Values[0]+"="+Name_Values[1]).replaceAll(" ","");//формирую строку, которую кладем в список, убираю пробелы перед и до знака "="
                     selectNameValue.append(optionPair);
@@ -59,31 +61,33 @@ form.addEventListener("submit",function (ev){
 })
 
 buttonSortByName.addEventListener("click", function (){
-    let arrNameValue = Array.from(select.options).map(value => value.value.split("="));
-    let arrName = arrNameValue.map(value => value[0]).sort((a,b) =>a.localeCompare(b, undefined, { numeric: true}));//sensitivity: 'base'
-    console.log(arrName);
-
-
-
-
-    // let arrOptions = [];
-    // for (const opt of select.options) {
-    //     arrOptions.push(opt.value);
-    // }
-    // console.log(arrOptions);
-
-
-    // let arrName = [];
-    // for (const nameValue of arrOptions) {
-    //     let numberQ = nameValue.indexOf("=",0);
-    //     let name = nameValue.slice(0,numberQ);
-    //     arrName.push(name);
-    // }
-    // console.log(arrName);
-
-
-// let slice =  joinArrs.slice(0,3); //Вырезает кусок массива по указанным индексам - с первого указанного по последний НЕ включая последний указанный индекс, изначальный массив НЕ меняется
-
+    let arrNameValue = Array.from(select.options).map(value => value.value.split("="));//получаю массив, в котором каждый элемент это массив из двух элементов - имя и значение
+    let arrSortNameValue =arrNameValue.sort((a,b) => a[0].localeCompare(b[0], undefined, { numeric: true})).map(value => value[0]+"="+value[1]); //сортирую массив по элементам с именем по методу localeCompare, локаль прописываю как undefined чтобы метод использовал системную/браузерную локаль, потому что в настройках проверки синтаксиса перед добавлением пары у меня указано принимать буквенно-цифровые символы любого алфавита; в обьекте настроек для localeCompare указываю чтобы числа внутри строк сравнивались как числа. Мепом собираю новый массив, в элементы которого записываю строки вида имя=значение
+    select.innerHTML = "";
+    arrSortNameValue.forEach(value => {
+        let optionPair = document.createElement("option");
+        optionPair.innerText = value;
+        select.append(optionPair);
+    })
 })
+
+buttonSortByValue.addEventListener("click", function (){
+    let arrNameValue = Array.from(select.options).map(value => value.value.split("="));
+    let arrSortNameValue =arrNameValue.sort((a,b) => a[1].localeCompare(b[1], undefined, { numeric: true})).map(value => value[0]+"="+value[1]);
+    select.innerHTML = "";
+    arrSortNameValue.forEach(value => {
+        let optionPair = document.createElement("option");
+        optionPair.innerText = value;
+        select.append(optionPair);
+    })
+})
+
+buttonDelete.addEventListener("click", function (){
+    let arrSelectedOptions = Array.from(select.selectedOptions).forEach(value => value.remove());//создаю массив из коллекции DOM 'элементов option, так как в массиве лежат ссылки на эти элементы, то удаляю их прямо из массива
+})
+
+
+
+
 
 
