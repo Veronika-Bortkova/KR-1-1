3ad7fccdf837cd138d5955ad0fd321c729a11930
Veronika-Bortkova <shishiga79@gmail.com>
2026-02-23 16:37:39 +0200

    finish-2

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let form = document.getElementById("formNameValuePair");
 let selectNameValue = document.getElementById("myList");
 let inputNameValuePair = document.getElementById("InputNameValuePair");
-let buttonAdd = document.getElementById("buttonAdd");
+// let buttonAdd = document.getElementById("buttonAdd");
 let DivInCorrectFormat = document.getElementById("inCorrectFormat");
 let btns = document.getElementsByTagName("button");
 let input = document.getElementById("InputNameValuePair");
@@ -13,11 +13,11 @@ let buttonDelete = document.getElementById("delete");
 let select = document.getElementById("myList");
 
 
-    input.addEventListener("dblclick", function (){//настраиваю, чтобы по двойному щелчку ЛКМ выделялось все содержание инпута
+    input.addEventListener("dblclick", function (){//Налаштовую, щоб подвійним кліком ЛКМ виділявся весь вміст інпута
         this.select();
     });
 if ((navigator.userAgentData && navigator.userAgentData.mobile||/Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
-)){//делаю проверку на то с какого устройства открыто приложение, что бы поставить разный текст у поля и кнопок на декстопе и мобильных устройствах (из-за того, что селект мультиплай отображается (заменяется) на мобильных браузерах не так, как на декстопе). Сделать это без джаваскрипта только брекпоинтами не получается, потому что брекпоинты мобильных устройств и лаптопов пересекаются. Делаю проверку по userAgentData и по userAgent потому что userAgentData поддерживается не всеми браузерами
+)){//Роблю перевірку, з якого пристрою відкрито застосунок, щоб поруч із полем та кнопками встановлювався різний текст на десктопі й мобільних пристроях (через те, що select multiply відображається/замінюється в мобільних браузерах не так, як на десктопі). Зробити це лише брейкпоінтами без JavaScript не виходить, бо брейкпоінти мобільних пристроїв і ноутбуків перетинаються. Виконую перевірку через userAgentData та через userAgent, оскільки userAgentData підтримується не всіма браузерами.
     divDeleteText.innerText = "(Delete selected)";
     pTexForFieldAndroid.innerText = "(Tap the field below ↓ to view the full list and select pairs)";
 } else {
@@ -26,7 +26,7 @@ if ((navigator.userAgentData && navigator.userAgentData.mobile||/Android|iPhone|
 }
 
 for (const btn of btns) {
-    btn.addEventListener("touchstart", () => {// настройки скейла кнопок при нажатии прописанные в css срабатывают не во всех браузерах, поэтому добавляю здесь
+    btn.addEventListener("touchstart", () => {// Налаштування масштабу кнопок при натисканні, прописані в CSS, спрацьовують не у всіх мобільних браузерах, тому додаю це тут
         btn.style.transform = "scale(0.8)";
     });
     btn.addEventListener("touchend", () => {
@@ -41,30 +41,30 @@ form.addEventListener("submit",function (ev){
     let value = inputNameValuePair.value;
     let arrNumberEqualSigns = [];
         for (const element of value) {
-            if (element === "=") {//проверяю есть ли во введенной строке знак "="
+            if (element === "=") {//Перевіряю, чи є у введеному рядку знак "="
                 arrNumberEqualSigns.push(element);
             }
         }
             if (arrNumberEqualSigns.length!=1){
-                DivInCorrectFormat.innerText = "Enter the information in the correct format";// если в строке нет знака "=" или их больше одного, выдаю сообщение о том, чтобы ввели текст в правильном формате
+                DivInCorrectFormat.innerText = "Enter the information in the correct format";// Якщо у рядку немає знака "=" або їх більше одного, видаю повідомлення про те, щоб ввели текст у правильному форматі
             } else {
                 let Name_Values = value.split("=");
-                if (!/^[\p{L}\p{N}][\p{L}\p{N}]*\s*$/u.test(Name_Values[0]) || !/^\s*[\p{L}\p{N}]+$/u.test(Name_Values[1])){//делаю проверку строк до знака равно и после. Строка находящаяся до знака "=" должна начинаться только с буквенно-цифровых символов, далее содержать любое (в том числе ноль) кол-во буквенно-цифровых символов, конец строки - любое кол-во пробелов. После знака "=" строка должна начинаться с любого кол-ва пробелов (в том числе ноль - ни одного пробела), далее любое кол-во буквенно-цифровых символов, но не менее одного символа. Юникод флаг для того чтобы работало для букв и чисел любого алфавита.
+                if (!/^[\p{L}\p{N}][\p{L}\p{N}]*\s*$/u.test(Name_Values[0]) || !/^\s*[\p{L}\p{N}]+$/u.test(Name_Values[1])){//роблю перевірку рядків до знака рівності та після. Рядок, що знаходиться до знака "=", має починатися лише з буквено‑цифрових символів, далі містити будь‑яку (в тому числі нуль) кількість буквено‑цифрових символів, кінець рядка — будь‑яку кількість пробілів. Після знака "=" рядок має починатися з будь‑якої кількості пробілів (у тому числі нуль — жодного пробілу), далі будь‑яка кількість буквено‑цифрових символів, але не менше одного символу. Unicode флаг потрібен для того, щоб працювало для букв і чисел будь‑якого алфавіту
                     DivInCorrectFormat.innerText = "Enter the information in the correct format";
                  } else{
-                    DivInCorrectFormat.innerText =" ";//убираю сообщение о том, чтобы ввели текст в правильном формате
+                    DivInCorrectFormat.innerText =" ";//прибираю повідомлення про те, щоб ввели текст у правильному форматі
                     let optionPair = document.createElement("option");
-                    optionPair.innerText =(Name_Values[0]+"="+Name_Values[1]).replaceAll(" ","");//формирую строку, которую кладем в список, убираю пробелы перед и до знака "="
+                    optionPair.innerText =(Name_Values[0]+"="+Name_Values[1]).replaceAll(" ","");//формую рядок, який кладемо в список, прибираю пробіли перед і до знака "="
                     selectNameValue.append(optionPair);
                      }
             }
 })
 
 buttonSortByName.addEventListener("click", function (){
-    let arrNameValue = Array.from(select.options).map(value => value.value.split("="));//получаю массив, в котором каждый элемент это массив из двух элементов - имя и значение
-    let arrSortNameValue =arrNameValue.sort((a,b) => a[0].localeCompare(b[0], undefined, { numeric: true})).map(value => value[0]+"="+value[1]); //сортирую массив по элементам с именем по методу localeCompare, локаль прописываю как undefined чтобы метод использовал системную/браузерную локаль, потому что в настройках проверки синтаксиса перед добавлением пары у меня указано принимать буквенно-цифровые символы любого алфавита; в обьекте настроек для localeCompare указываю чтобы числа внутри строк сравнивались как числа. Мепом собираю новый массив, в элементы которого записываю строки вида имя=значение
+    let arrNameValues = Array.from(select.options).map(value => value.value.split("="));//отримую масив, у якому кожен елемент — це масив із двох елементів: ім’я та значення
+    let arrSortNameValues =arrNameValues.sort((a,b) => a[0].localeCompare(b[0], undefined, { numeric: true})).map(value => value[0]+"="+value[1]); //сортую масив за елементами з іменем методом localeCompare, локаль прописую як undefined, щоб метод використовував системну/браузерну локаль, тому що в налаштуваннях перевірки синтаксису перед додаванням пари в мене вказано приймати буквено‑цифрові символи будь‑якого алфавіту; в об’єкті налаштувань для localeCompare зазначаю, щоб числа всередині рядків порівнювалися як числа. Maпом збираю новий масив, у елементи якого записую рядки виду ім’я=значення
     select.innerHTML = "";
-    arrSortNameValue.forEach(value => {
+    arrSortNameValues.forEach(value => {
         let optionPair = document.createElement("option");
         optionPair.innerText = value;
         select.append(optionPair);
@@ -72,10 +72,10 @@ buttonSortByName.addEventListener("click", function (){
 })
 
 buttonSortByValue.addEventListener("click", function (){
-    let arrNameValue = Array.from(select.options).map(value => value.value.split("="));
-    let arrSortNameValue =arrNameValue.sort((a,b) => a[1].localeCompare(b[1], undefined, { numeric: true})).map(value => value[0]+"="+value[1]);
+    let arrNameValues = Array.from(select.options).map(value => value.value.split("="));
+    let arrSortNameValues =arrNameValues.sort((a,b) => a[1].localeCompare(b[1], undefined, { numeric: true})).map(value => value[0]+"="+value[1]);
     select.innerHTML = "";
-    arrSortNameValue.forEach(value => {
+    arrSortNameValues.forEach(value => {
         let optionPair = document.createElement("option");
         optionPair.innerText = value;
         select.append(optionPair);
@@ -83,7 +83,7 @@ buttonSortByValue.addEventListener("click", function (){
 })
 
 buttonDelete.addEventListener("click", function (){
-    let arrSelectedOptions = Array.from(select.selectedOptions).forEach(value => value.remove());//создаю массив из коллекции DOM 'элементов option, так как в массиве лежат ссылки на эти элементы, то удаляю их прямо из массива
+    Array.from(select.selectedOptions).forEach(value => value.remove());//cтворюю масив із колекції DOM‑елементів option, оскільки в масиві лежать посилання на ці елементи, то видаляю їх прямо з масиву
 })
 
 
